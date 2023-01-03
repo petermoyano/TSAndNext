e359cbf667a722fe8a9f78c62142b91a4057d8bc
Peter M <pmoyano@solvd.com>
2023-01-03 12:23:30 -0300

    form functionality with types

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Calculator from "./components/Calculator";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="wrapper">
-      <Calculator />
+      {/* <Calculator /> */}
+      <Form />
     </div>
   );
 }
--- calculator/src/components/Form.tsx
+++ calculator/src/components/Form.tsx
@@ -1,7 +1,35 @@
-import { FC } from "react";
+import { FC, useState, ChangeEventHandler, FormEventHandler } from "react";
+import Button from "./Button";
+
 interface Props {}
+
 const Form: FC<Props> = (): JSX.Element => {
-  return <></>;
+  const [value, setValue] = useState("");
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex-col">
+      <input
+        className="border padding-10"
+        type="text"
+        value={value}
+        onChange={handleChange}
+      />
+      <div className="padding-10">
+        <Button value="Submit" />
+      </div>
+    </form>
+  );
 };
 
 export default Form;
